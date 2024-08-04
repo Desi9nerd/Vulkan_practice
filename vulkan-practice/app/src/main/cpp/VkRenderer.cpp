adf8c95328d0d086fc218adc3fce522243d3df5f
Desi9nerd <swpark514@gmail.com>
2024-08-04 12:18:12 +0900

    Vulkan Instance

---
+++ vulkan-practice/app/src/main/cpp/VkRenderer.cpp
@@ -0,0 +1,72 @@
+// MIT License
+//
+// Copyright (c) 2024 Daemyung Jang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <cassert>
+#include <array>
+#include <vector>
+
+#include "VkRenderer.h"
+#include "VkUtil.h"
+#include "AndroidOut.h"
+
+using namespace std;
+
+VkRenderer::VkRenderer() {
+
+    // VkApplicationInfo 구조체 정의
+    VkApplicationInfo applicationInfo{
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "Practice Vulkan",
+        .applicationVersion = VK_MAKE_API_VERSION(0, 0, 1, 0),
+        .apiVersion = VK_MAKE_API_VERSION(0, 1, 3, 0)
+    };
+
+    // 사용할 수 있는 레이어를 얻어온다.
+    uint32_t instanceLayerCount;
+    VK_CHECK_ERROR(vkEnumerateInstanceLayerProperties(&instanceLayerCount, nullptr));
+
+    vector<VkLayerProperties> instanceLayerProperties(instanceLayerCount);
+    VK_CHECK_ERROR(vkEnumerateInstanceLayerProperties(&instanceLayerCount,
+                                                      instanceLayerProperties.data()));
+
+    // 활성화할 레이어의 이름을 배열로 만든다.
+    vector<const char*> instanceLayerNames;
+    for (const auto &layerProperty : instanceLayerProperties) {
+        instanceLayerNames.push_back(layerProperty.layerName);
+    }
+
+    // sType: 구조체의 타입, pApplicationInfo: 어플리케이션의 이름
+    // enabledLayerCount, ppEnableLayerNames: 사용할 레이어의 정보를 정의
+    VkInstanceCreateInfo instanceCreateInfo{
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &applicationInfo,
+        .enabledLayerCount = static_cast<uint32_t>(instanceLayerNames.size()),
+        .ppEnabledLayerNames = instanceLayerNames.data()
+    };
+
+    // vkCreateInstance로 인스턴스 생성. 생성된 인스턴스가 mInstance에 쓰여진다.
+    VK_CHECK_ERROR(vkCreateInstance(&instanceCreateInfo, nullptr, &mInstance));
+}
+
+VkRenderer::~VkRenderer() {
+    vkDestroyInstance(mInstance, nullptr);
+}
