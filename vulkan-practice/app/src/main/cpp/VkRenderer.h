88050f1f1032e751b8936231c6c91f94018cab0b
Desi9nerd <swpark514@gmail.com>
2024-08-14 13:10:36 +0900

    Color Animation

--- vulkan-practice/app/src/main/cpp/VkRenderer.h
+++ vulkan-practice/app/src/main/cpp/VkRenderer.h
@@ -24,9 +24,22 @@
 
 class VkRenderer {
 public:
-    VkRenderer();
+    explicit VkRenderer(ANativeWindow* window);
     ~VkRenderer();
+
+    void render();
 
 private:
     VkInstance mInstance;
+    VkPhysicalDevice mPhysicalDevice;
+    uint32_t mQueueFamilyIndex;
+    VkDevice mDevice;
+    VkQueue mQueue;
+    VkSurfaceKHR mSurface;
+    VkSwapchainKHR mSwapchain;
+    std::vector<VkImage> mSwapchainImages;
+    VkCommandPool mCommandPool;
+    VkCommandBuffer mCommandBuffer;
+    VkFence mFence;
+    VkClearColorValue mClearColorValue{.float32{0.6431, 0.7765, 0.2235, 1.0}};
 };
